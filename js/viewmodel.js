0204ef8b14c7eac89c67e6a3dd1eedbcba828949
Michael Ryden <m_ryden@yahoo.com>
2017-12-06 20:02:08 -0600

    closure for marker and infowindow

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,8 +1,45 @@
 var map;
+
+
+
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 32.9413599, lng: -96.6304932},
       zoom: 12
     });
-  }
+
+    var markers = [
+        {title: 'breckinridge park',
+        position: {lat: 32.9968091, lng: -96.6281827},
+        map: map,
+        id: 0},
+        {title: 'home',
+        position: {lat: 32.9413599, lng: -96.6304932},
+        map: map,
+        id: 1},
+        {title: 'firewheel shopping center',
+        position: {lat: 32.9527034, lng: -96.6145097},
+        map: map,
+        id: 2}
+    ];
+
+    for (i=0; i<=markers.length; i++){
+        //closure to add marker and infowindow for each location
+        var markerInfo = (function(cMarkers){
+            var content = '<div>' + markers[i].title + '</div>';
+            var infowindow = new google.maps.InfoWindow({
+                content: content
+            });
+            var marker = new google.maps.Marker({
+                title: cMarkers[i].title,
+                position: cMarkers[i].position,
+                map: cMarkers[i].map,
+                id: cMarkers[i].id
+            });
+            // closures
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+        });})(markers);
+    };
+};
 
